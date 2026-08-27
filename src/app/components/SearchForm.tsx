835adaa499e2b5a90418b456891537012c5d2e77
yunajoe <yunajoe@gmail.com>
2026-08-27 20:29:39 +0900

    style:bread crumb

---
+++ src/app/components/SearchForm.tsx
@@ -0,0 +1,13 @@
+export default function SearchForm({
+  initialQuery,
+}: {
+  initialQuery?: string;
+}) {
+  // TODO 1: 자바스크립트(useState) 없이 HTML <form>과 action, name="q" 속성을 활용해 검색창을 구현하세요.
+  // TODO 2: initialQuery를 받아 검색창의 defaultValue로 설정하여 상태를 유지하세요.
+  return (
+    <div className="p-4 border border-red-300 bg-red-50 text-red-600 rounded-md">
+      여기에 SearchForm 컴포넌트를 구현해주세요.
+    </div>
+  );
+}
--- src/app/posts/[[...category]]/page.tsx
+++ src/app/posts/[[...category]]/page.tsx
@@ -26,33 +26,68 @@ const allPosts = [
 ];
 
 interface PageProps {
-  params: Promise<{ category?: string }>;
+  params: Promise<{ category?: string[] }>;
   searchParams: Promise<{ sort?: string; q?: string }>;
 }
 async function page({ params, searchParams }: PageProps) {
-  const result = await Promise.all([params, searchParams]);
-
+  const [p, sp] = await Promise.all([params, searchParams]);
+  const { category = [] } = p;
+  //   # 1. 초기 화면 (/posts 접속 시)
   // [검색창: 🔍 검색어 입력... ] [조회수 높은 순 ⬆️] [초기화 🔄]
+  // -----------------------------------------
+  // [홈] ⬅️ 빵 부스러기 네비게이션
+  // 📌 전체 게시글
+  // - Next.js 15 라우팅 가이드 (조회수: 150)
+  // - React 19 훅스 파헤치기 (조회수: 230)
+  // - Node.js 백엔드 기초 (조회수: 90)
+
+  // # 2. 카테고리 진입 및 검색 (/posts/frontend/react?q=훅스 접속 시)
+  // [검색창: 🔍 훅스 ] [조회수 높은 순 ⬆️] [초기화 🔄]
+  // -----------------------------------------
+  // [홈] > [frontend] > [react] ⬅️ 배열 파라미터로 자동 생성됨
+  // 📌 검색 결과: "훅스"
+  // - React 19 훅스 파헤치기 (조회수: 230)
 
   return (
     <div className="">
+      {/* 네비게이션 */}
       <nav className="w-full border-2 border-solid p-4 flex justify-center gap-10">
-        <div className="w-3/5 flex gap-4 bg-blue-100">
+        <div className="w-3/5 flex gap-4">
           <input
             type="text"
             placeholder="🔍 검색어 입력..."
             className="border-2 border-blue-400 w-3/5 rounded-sm"
           />
-          <button className="w-28 border-2 border-gray-800">검색</button>
+          <button className="w-28 border-2 border-gray-800 bg-black text-white">
+            검색
+          </button>
         </div>
 
-        <div className="w-2/5 bg-blue-100 flex items-center justify-center gap-4">
-          <button className="border-2 border-gray-800 w-30">
+        <div className="w-2/5 flex items-center justify-center gap-4">
+          <button className="border-2 border-gray-800 w-30 cursor-pointer p-2">
             조회 높은 순
           </button>
-          <button className="border-2 border-gray-800 w-30">리셋</button>
+          <button className="border-2 border-gray-800 w-30 cursor-pointer p-2">
+            리셋
+          </button>
         </div>
       </nav>
+
+      {/* bread crumb */}
+      <ul className="list-none flex px-6 py-10">
+        <span className="text-3xl font-bold">홈</span>
+        {category.length > 0 && (
+          <div>
+            {category.map((item, index) => {
+              return (
+                <span key={index} className="text-3xl">
+                  {`> ${item}`}
+                </span>
+              );
+            })}
+          </div>
+        )}
+      </ul>
     </div>
   );
 }
