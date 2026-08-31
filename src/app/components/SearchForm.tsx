6b28318ab949fcda4e99b1f11c440e6b60abe7d9
yunajoe <yunajoe@gmail.com>
2026-08-31 13:16:03 +0900

    실습:중첩 카테고리

--- src/app/components/SearchForm.tsx
+++ src/app/components/SearchForm.tsx
@@ -1,13 +1,45 @@
+"use client";
 export default function SearchForm({
   initialQuery,
+  url,
+  sort,
 }: {
   initialQuery?: string;
+  url?: string;
+  sort?: string;
 }) {
-  // TODO 1: 자바스크립트(useState) 없이 HTML <form>과 action, name="q" 속성을 활용해 검색창을 구현하세요.
-  // TODO 2: initialQuery를 받아 검색창의 defaultValue로 설정하여 상태를 유지하세요.
   return (
-    <div className="p-4 border border-red-300 bg-red-50 text-red-600 rounded-md">
-      여기에 SearchForm 컴포넌트를 구현해주세요.
-    </div>
+    /**
+     * <form>은 submission 시 <input> 태그의 name 속성을 Query Key로 사용한다.
+     * 즉, 검색어(input)에 리액트를 치고 검색 버튼을 누르면은  posts?q=리액트 로 된다는 말.
+     */
+
+    <form
+      action={url}
+      method="GET"
+      className="w-full flex items-center gap-2"
+      onSubmit={(e) => {
+        const form = e.currentTarget; // form 태그 영역
+        const input = form.elements.namedItem("q") as HTMLInputElement;
+        if (input && !input.value.trim()) {
+          input.name = "";
+        }
+      }}
+    >
+      {sort && <input type="hidden" name="sort" value={sort} />}
+      <input
+        name="q"
+        type="text"
+        placeholder="🔍 검색어 입력..."
+        className="border-2 border-blue-400 rounded-sm w-full"
+        defaultValue={initialQuery}
+      />
+      <button
+        type="submit"
+        className="w-28 border-2 border-gray-800 bg-black text-white"
+      >
+        검색
+      </button>
+    </form>
   );
 }
--- src/app/posts/[[...category]]/page.tsx
+++ src/app/posts/[[...category]]/page.tsx
@@ -1,3 +1,6 @@
+import SearchForm from "@/src/app/components/SearchForm";
+import Link from "next/link";
+
 const allPosts = [
   {
     id: 1,
@@ -23,6 +26,12 @@ const allPosts = [
     path: ["backend", "nodejs"],
     views: 90,
   },
+  {
+    id: 5,
+    title: "JAVA 백엔드 기초",
+    path: ["backend", "java"],
+    views: 120,
+  },
 ];
 
 interface PageProps {
@@ -32,62 +41,81 @@ interface PageProps {
 async function page({ params, searchParams }: PageProps) {
   const [p, sp] = await Promise.all([params, searchParams]);
   const { category = [] } = p;
-  //   # 1. 초기 화면 (/posts 접속 시)
-  // [검색창: 🔍 검색어 입력... ] [조회수 높은 순 ⬆️] [초기화 🔄]
-  // -----------------------------------------
-  // [홈] ⬅️ 빵 부스러기 네비게이션
-  // 📌 전체 게시글
-  // - Next.js 15 라우팅 가이드 (조회수: 150)
-  // - React 19 훅스 파헤치기 (조회수: 230)
-  // - Node.js 백엔드 기초 (조회수: 90)
+  const { sort = "", q = "" } = sp;
+  let baseUrl = `/posts/${category.join("/")}`; // posts/frontend/react
+  let searchParamsUrl = baseUrl;
 
-  // # 2. 카테고리 진입 및 검색 (/posts/frontend/react?q=훅스 접속 시)
-  // [검색창: 🔍 훅스 ] [조회수 높은 순 ⬆️] [초기화 🔄]
-  // -----------------------------------------
-  // [홈] > [frontend] > [react] ⬅️ 배열 파라미터로 자동 생성됨
-  // 📌 검색 결과: "훅스"
-  // - React 19 훅스 파헤치기 (조회수: 230)
+  let filterPosts = [...allPosts];
+
+  // 데이터
+  // sort (정렬)
+  if (sort === "desc") {
+    filterPosts.sort((a, b) => b.views - a.views);
+  }
+  // q (검색어)
+  if (q.trim()) {
+    filterPosts = filterPosts.filter((item) => item.title.includes(q.trim()));
+  }
+
+  const sortHref = q ? `${baseUrl}?q=${q}&sort=desc` : `${baseUrl}?sort=desc`;
 
   return (
     <div className="">
-      {/* 네비게이션 */}
       <nav className="w-full border-2 border-solid p-4 flex justify-center gap-10">
-        <div className="w-3/5 flex gap-4">
-          <input
-            type="text"
-            placeholder="🔍 검색어 입력..."
-            className="border-2 border-blue-400 w-3/5 rounded-sm"
-          />
-          <button className="w-28 border-2 border-gray-800 bg-black text-white">
-            검색
-          </button>
-        </div>
+        <SearchForm url={baseUrl} initialQuery={q} sort={sort} />
 
         <div className="w-2/5 flex items-center justify-center gap-4">
-          <button className="border-2 border-gray-800 w-30 cursor-pointer p-2">
-            조회 높은 순
-          </button>
-          <button className="border-2 border-gray-800 w-30 cursor-pointer p-2">
-            리셋
-          </button>
+          <Link
+            href={sortHref}
+            className="px-4 py-2 border border-gray-300 bg-white rounded-md hover:bg-gray-100"
+          >
+            조회수 높은 순 ⬆️
+          </Link>
+          <Link
+            href="/posts"
+            className="px-4 py-2 border border-gray-300 bg-white rounded-md hover:bg-gray-100"
+          >
+            초기화 🔄
+          </Link>
         </div>
       </nav>
 
-      {/* bread crumb */}
-      <ul className="list-none flex px-6 py-10">
-        <span className="text-3xl font-bold">홈</span>
-        {category.length > 0 && (
-          <div>
-            {category.map((item, index) => {
-              return (
-                <span key={index} className="text-3xl">
-                  {`> ${item}`}
-                </span>
-              );
-            })}
+      <div className="px-6 py-10 flex flex-col gap-10">
+        {/* bread crumb */}
+        <ul className="list-none flex">
+          <span className="text-3xl font-bold">홈</span>
+          {category.length > 0 && (
+            <div>
+              {category.map((item, index) => {
+                return (
+                  <span key={index} className="text-3xl">
+                    {`> ${item}`}
+                  </span>
+                );
+              })}
+            </div>
+          )}
+        </ul>
+
+        {/* 내용*/}
+        <div className="flex flex-col gap-8">
+          <h1 className="text-3xl">
+            {q ? `📌 검색 결과: "${q}"` : "📌 게시글 목록"}
+          </h1>
+          <div className="grid grid-cols-2 gap-4">
+            {filterPosts.map((item) => (
+              <div
+                key={item.id}
+                className="border-2 border-gray-300 rounded-md p-4 w-100"
+              >
+                <p>{item.path.join(" > ")}</p>
+                <p>{item.title}</p>
+                <p>조회수: {item.views}</p>
+              </div>
+            ))}
           </div>
-        )}
-      </ul>
+        </div>
+      </div>
     </div>
   );
 }
